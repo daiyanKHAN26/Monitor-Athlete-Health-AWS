1cef2e7593c2f3e4d0e0aac9440087c980af00c0
Daiyan Khan <75927694+daiyanKHAN26@users.noreply.github.com>
2024-11-25 06:28:06 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    AWS_REGION: 'us-east-1',
-    AWS_ACCESS_KEY_ID: 'AKIAXKPUZLMSG2SUA5GG',
-    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_REGION: 
+    AWS_ACCESS_KEY_ID: 
+    AWS_SECRET_ACCESS_KEY: 
   };
